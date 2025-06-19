941c51310bac088d035c97e86d72ed534ebfaac2
mel <beladeyakosso.keturamelissa@gmail.com>
2025-06-19 12:03:13 +0200

    first commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMovement : MonoBehaviour
+{
+
+
+    public GameObject character;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(character.transform.position.x + 5.5f, 0, -10);
+    }
+}
---
+++ Assets/Scripts/cat_walk.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class cat_walk : MonoBehaviour
+{
+    public float speed = 30f;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.deltaTime);
+
+        // Détection de mouvement
+        bool isWalking = movement != Vector2.zero;
+
+        // Mise à jour de l'animation
+        animator.SetBool("isWalking", isWalking);
+    }
+}
